b1382b37595f10bd11ecf988aeeddfab603e4962
saikumarbandi4 <131740974+saikumarbandi4@users.noreply.github.com>
2023-04-26 17:29:46 +0530

    Update Copy_Variable_Group.ps1

--- Copy_Variable_Group.ps1
+++ Copy_Variable_Group.ps1
@@ -1,56 +1,9 @@
-param ($srcproj,$grpname,$dstproj, $PAT)
+$connectionToken="$(pat)"
+$base64AuthInfo= [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes(":$($connectionToken)"))
+$URL = "https://dev.azure.com/{Org name}/{Project name}/_apis/distributedtask/variablegroups?groupIds={Variable group ID}&api-version=6.0-preview.2"
+$Result = Invoke-RestMethod -Uri $URL -Headers @{authorization = "Basic $base64AuthInfo"} -Method Get 
+ 
 
-#$AllProtocols = [System.Net.SecurityProtocolType]'Tls12'
-#[System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
+$Variable = $Result.value.variables | ConvertTo-Json -Depth 100
 
-$base64AuthInfo = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes(":$($PAT)"))
-
-$variablegroups_url = "https://dev.azure.com/vgroupframework/$($srcproj)/_apis/distributedtask/variablegroups?groupName=$($grpname)&api-version=5.1-preview.1"
-
-$vargroups = Invoke-RestMethod -Uri $variablegroups_url -Headers @{Authorization = ("Basic {0}" -f $base64AuthInfo)} -Method 'GET'
-
-$groupresult = $vargroups | ConvertTo-Json
-
-$jsonid = $groupresult | ConvertFrom-Json
-
-#Write-Host $groupresult
-
-#Write-Host $jsonid
-
-$id = $jsonid.value[0].id #first matching record id
-
-#Get the variable group in projectA
-
-$url = "https://dev.azure.com/vgroupframework/$($srcproj)/_apis/distributedtask/variablegroups/$($id)?api-version=5.1-preview.1"
-
-$result = Invoke-RestMethod -Uri $url -Headers @{Authorization = ("Basic {0}" -f $base64AuthInfo)} -Method get -ContentType "application/json"
-
-# check if variable group already exists
-$dest_variablegroups_url = "https://dev.azure.com/vgroupframework/$($dstproj)/_apis/distributedtask/variablegroups?groupName=$($grpname)&api-version=5.1-preview.1"
-
-$dest_vargroups = Invoke-RestMethod -Uri $dest_variablegroups_url -Headers @{Authorization = ("Basic {0}" -f $base64AuthInfo)} -Method 'GET'
-
-$dest_groupresult = $dest_vargroups | ConvertTo-Json
-
-$dest_jsonid = $dest_groupresult | ConvertFrom-Json
-
-$updateurl = ""
-$method = ""
-$dest_grpId =[int]$dest_jsonid.value[0].id
-
-#Write-Host $dest_grpId
-
-if($dest_grpId -gt 0)
-{
-    $updateurl = "https://dev.azure.com/vgroupframework/$($dstproj)/_apis/distributedtask/variablegroups/$($dest_grpId)?api-version=5.1-preview.1"
-    $method = "PUT"
-}
-else
-{
-    # Call add variable group rest api to add variable group in ProjectB
-    $updateurl = "https://dev.azure.com/vgroupframework/$($dstproj)/_apis/distributedtask/variablegroups?api-version=5.1-preview.1"
-    $method = "POST"
-}
-
-$body = $result | ConvertTo-Json -Depth 10
-Invoke-RestMethod -Uri $updateurl -Headers @{Authorization = "Basic {0}" -f $base64AuthInfo} -ContentType "application/json" -Method $method -Body $body
+Write-Host $Variable
